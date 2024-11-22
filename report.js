de96697235b26d2db4c2a404f46e742e479699fb
Abhik roy <abhikfiem@gmail.com>
2024-11-22 18:17:44 +0530

    add report

--- report.js
+++ report.js
@@ -1,116 +1,14 @@
 document.getElementById("submitReport").addEventListener("click", function () {
-  // toggleDiv();
-  var reportDate = document.getElementById("ReportsDate").value;
-  if (qrCode) {
-    fetch("reports.php?reportDate=" + reportDate)
+  let rptdte = document.getElementById("ReportsDate").value;
+  if (rptdte) {
+    fetch("reports.php?reportDate=" + rptdte)
       .then((response) => response.json())
       .then((data) => {
-        if (data.message) {
-          document.getElementById("result").innerHTML =
-            '<div class="alert alert-danger">FE number is  not present</div>';
-        } else {
-          // Function to add days to a given date
-          function addDays(dateString, days) {
-            const date = new Date(dateString);
-            date.setDate(date.getDate() + days);
-            return date.toISOString().split("T")[0]; // Return formatted date (YYYY-MM-DD)
-          }
-          // Adding 30 days to the checking date
-          // console.log(data.build_date)
-          function diffday(dateString) {
-            const today = new Date();
-            const checkdate = new Date(dateString);
-            const diff = today - checkdate; // Calculate the difference in milliseconds
-            const differenceInDays = Math.floor(diff / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
-            // Check if the difference is negative or positive
-            if (differenceInDays < 0) {
-              return {
-                message: "    Due in " + Math.abs(differenceInDays) + " days",
-                className: "due",
-              };
-            } else {
-              return {
-                message: "    Overdue by " + differenceInDays + " days",
-                className: "overdue",
-              };
-            }
-          }
-          let next_checking_date_limit = 90;
-          let next_refilling_date_limit = 365;
-          let next_hydraulic_test_date_limit = 1095;
-
-          const next_checking_date =
-            data.checking_date !== "0000-00-00"
-              ? addDays(data.checking_date, next_checking_date_limit)
-              : addDays(data.build_date, next_checking_date_limit);
-          // console.log(diffday(next_checking_date));
-          const next_refilling_date =
-            data.refilling_date !== "0000-00-00"
-              ? addDays(data.refilling_date, next_refilling_date_limit)
-              : addDays(data.build_date, next_refilling_date_limit);
-          const next_hydraulic_test_date =
-            data.hydraulic_test_date !== "0000-00-00"
-              ? addDays(
-                  data.hydraulic_test_date,
-                  next_hydraulic_test_date_limit
-                )
-              : addDays(data.build_date, next_hydraulic_test_date_limit);
-          if (data.message) {
-            document.getElementById("result").innerHTML =
-              '<div class="alert alert-danger">' + data.message + "</div>";
-          } else {
-            document.getElementById("result").innerHTML = `
-                                  <div class="card">
-                                      <div class="card-header">
-                                          Fire Extinguisher Information
-                                      </div>
-                                      <div class="card-body">
-              
-                                  <ul class="list-group">
-                                      <li class="list-group-item">Coach Number: ${
-                                        data.coach_number
-                                      }</li>
-                                      <li class="list-group-item">Build Date: ${
-                                        data.build_date
-                                      }</li>
-                                      <li class="list-group-item">Checking Date: ${
-                                        data.checking_date
-                                      }</li>
-                                      <li class="list-group-item ${
-                                        diffday(next_checking_date).className
-                                      }">Next Checking Date due on : ${next_checking_date} ${
-              diffday(next_checking_date).message
-            }</li>
-              
-                                      <li class="list-group-item">Refilling Date: ${
-                                        data.refilling_date
-                                      }</li>
-                                      <li class="list-group-item ${
-                                        diffday(next_refilling_date).className
-                                      }">Next Refilling Date due on : ${next_refilling_date} ${
-              diffday(next_refilling_date).message
-            }</li>
-                                      <li class="list-group-item">Hydraulic Test Date: ${
-                                        data.hydraulic_test_date
-                                      }</li>
-                                      <li class="list-group-item ${
-                                        diffday(next_hydraulic_test_date)
-                                          .className
-                                      }">Next Hydraulic Test Date due on : ${next_hydraulic_test_date} ${
-              diffday(next_hydraulic_test_date).message
-            }</li>
-                                      <li class="list-group-item">Comments : ${
-                                        data.status
-                                      }</li>
-                                  </ul>
-                                      </div>
-                                  </div>
-                              `;
-          }
-        }
+        console.log(data);
+        document.getElementById("detailschecking").innerHTML = data;
       });
   } else {
     document.getElementById("result").innerHTML =
-      '<div class="alert alert-danger">Please Enter or Scan a QR code</div>';
+      '<div class="alert alert-danger">Please Enter a Date</div>';
   }
 });
--- reports.php
+++ reports.php
@@ -15,25 +15,96 @@ if ($conn->connect_error) {
 
 if (isset($_GET['reportDate'])) {
     $reportDate = $_GET['reportDate'];
-    $sql = "SELECT unique_number,coach_number,hydraulic_test_date,
+
+    $sql_checking_date = "SELECT unique_number,coach_number,checking_date,
+    CASE 
+        WHEN checking_date != '0000-00-00' THEN DATE_ADD(checking_date, INTERVAL 90 DAY)
+        ELSE DATE_ADD(build_date, INTERVAL 90 DAY)
+    END AS next_checking_date,
+    DATEDIFF(CURRENT_DATE(), (CASE 
+        WHEN checking_date != '0000-00-00' THEN DATE_ADD(checking_date, INTERVAL 90 DAY)
+        ELSE DATE_ADD(build_date, INTERVAL 90 DAY)
+    END)) AS day_difference
+FROM `fire_extinguishers`
+WHERE 
+    (CASE 
+        WHEN checking_date != '0000-00-00' THEN DATE_ADD(checking_date, INTERVAL 90 DAY)
+        ELSE DATE_ADD(build_date, INTERVAL 90 DAY)
+    END) <= '$reportDate'
+    ORDER by  day_difference DESC ;";
+    $result_checking_date = $conn->query($sql_checking_date);
+
+
+
+
+    // --------------------------------------------------------------------------- 
+    $sql_refilling_date = "SELECT unique_number,coach_number,refilling_date,
+        CASE 
+            WHEN refilling_date != '0000-00-00' THEN DATE_ADD(refilling_date, INTERVAL 365 DAY)
+            ELSE DATE_ADD(build_date, INTERVAL 365 DAY)
+        END AS next_refilling_date,
+        DATEDIFF(CURRENT_DATE(), (CASE 
+            WHEN refilling_date != '0000-00-00' THEN DATE_ADD(refilling_date, INTERVAL 365 DAY)
+            ELSE DATE_ADD(build_date, INTERVAL 365 DAY)
+        END)) AS day_difference
+    FROM `fire_extinguishers`
+    WHERE 
+        (CASE 
+            WHEN refilling_date != '0000-00-00' THEN DATE_ADD(refilling_date, INTERVAL 365 DAY)
+            ELSE DATE_ADD(build_date, INTERVAL 365 DAY)
+        END) <= '$reportDate'
+        ORDER by  day_difference DESC ;";
+    $result_refilling_date = $conn->query($sql_refilling_date);
+
+
+    // ----------------------------------------------------------------------------------------------
+
+    $sql_hydraulic_test_date = "SELECT unique_number,coach_number,hydraulic_test_date,
     CASE 
         WHEN hydraulic_test_date != '0000-00-00' THEN DATE_ADD(hydraulic_test_date, INTERVAL 1095 DAY)
         ELSE DATE_ADD(build_date, INTERVAL 1095 DAY)
-    END AS next_hydraulic_test_date
+    END AS next_hydraulic_test_date,
+    DATEDIFF(CURRENT_DATE(), (CASE 
+        WHEN hydraulic_test_date != '0000-00-00' THEN DATE_ADD(hydraulic_test_date, INTERVAL 1095 DAY)
+        ELSE DATE_ADD(build_date, INTERVAL 1095 DAY)
+    END)) AS day_difference
 FROM `fire_extinguishers`
 WHERE 
     (CASE 
         WHEN hydraulic_test_date != '0000-00-00' THEN DATE_ADD(hydraulic_test_date, INTERVAL 1095 DAY)
         ELSE DATE_ADD(build_date, INTERVAL 1095 DAY)
-    END) <= '$reportDate';";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        $data = $result->fetch_assoc();
-        echo json_encode($data);
-    } else {
-        echo json_encode(["message" => "No Report found"]);
-    }
+    END) <= '$reportDate'
+    ORDER by  day_difference DESC ;";
+    $result_hydraulic_test_date = $conn->query($sql_hydraulic_test_date);
 }
 $conn->close();
-?>
+
+
+echo '    <table>
+        <thead>
+            <tr>
+                <th>FE Number</th>
+                <th>Checking Date</th>
+                <th>Next Checking Date</th>
+                <th>Day\'s Pending</th>
+            </tr>
+        </thead>
+        <tbody>';
+
+if ($result_checking_date->num_rows > 0) {
+    // Output each row
+    while ($row = $result_checking_date->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["unique_number"] . "</td>";
+        echo "<td>" . $row["checking_date"] . "</td>";
+        echo "<td>" . $row["next_checking_date"] . "</td>";
+        echo "<td>" . $row["day_difference"] . "</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td colspan='4'>No records found</td></tr>";
+}
+echo '
+ </tbody>
+    </table>
+';
